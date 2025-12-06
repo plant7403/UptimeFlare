a062464cf3010476ff1b4765475786633904f70f
me <me@o.o>
2025-12-06 01:34:46 +0100

    let's goo

--- uptime.config.ts
+++ uptime.config.ts
@@ -2,18 +2,18 @@ import { MaintenanceConfig, PageConfig, WorkerConfig } from './types/config'
 
 const pageConfig: PageConfig = {
   // Title for your status page
-  title: "lyc8503's Status Page",
+  title: "Pak Unity Status",
   // Links shown at the header of your status page, could set `highlight` to `true`
   links: [
-    { link: 'https://github.com/lyc8503', label: 'GitHub' },
-    { link: 'https://blog.lyc8503.net/', label: 'Blog' },
-    { link: 'mailto:me@lyc8503.net', label: 'Email Me', highlight: true },
+    { link: 'https://github.com/', label: 'GitHub' },
+    { link: 'https://blog.pak.academy/', label: 'Blog' },
+    { link: 'mailto:contact@pak.academy', label: 'Email Me', highlight: true },
   ],
   // [OPTIONAL] Group your monitors
   // If not specified, all monitors will be shown in a single list
   // If specified, monitors will be grouped and ordered, not-listed monitors will be invisble (but still monitored)
   group: {
-    '🌐 Public (example group name)': ['foo_monitor'],
+    '🌐 Websites': ['pak.academy', 'expat.food'],
     '🔐 Private': ['test_tcp_monitor'],
   },
   // [OPTIONAL] Set the path to your favicon, default to '/favicon.png' if not specified
@@ -37,43 +37,31 @@ const workerConfig: WorkerConfig = {
   // passwordProtection: 'username:password',
   // Define all your monitors here
   monitors: [
-    // Example HTTP Monitor
     {
-      // `id` should be unique, history will be kept if the `id` remains constant
-      id: 'foo_monitor',
-      // `name` is used at status page and callback message
-      name: 'My API Monitor',
-      // `method` should be a valid HTTP Method
+      id: 'pak.academy',
+      name: 'Pak Academy Main Page',
       method: 'POST',
-      // `target` is a valid URL
       target: 'http://pak.academy',
-      // [OPTIONAL] `tooltip` is ONLY used at status page to show a tooltip
       tooltip: 'This is a tooltip for this monitor',
-      // [OPTIONAL] `statusPageLink` is ONLY used for clickable link at status page
       statusPageLink: 'https://pak.academy',
-      // [OPTIONAL] `hideLatencyChart` will hide status page latency chart if set to true
       hideLatencyChart: false,
-      // [OPTIONAL] `expectedCodes` is an array of acceptable HTTP response codes, if not specified, default to 2xx
-      expectedCodes: [200],
-      // [OPTIONAL] `timeout` in millisecond, if not specified, default to 10000
-      timeout: 10000,
-      // [OPTIONAL] headers to be sent
       headers: {
         'User-Agent': 'Uptimeflare',
-        // Authorization: 'Bearer YOUR_TOKEN_HERE',
       },
-      // [OPTIONAL] body to be sent
       body: 'Hello, world!',
-      // [OPTIONAL] if specified, the response must contains the keyword to be considered as operational.
-      //responseKeyword: 'success',
-      // [OPTIONAL] if specified, the response must NOT contains the keyword to be considered as operational.
-      //responseForbiddenKeyword: 'bad gateway',
-      // [OPTIONAL] if specified, will call the check proxy to check the monitor, mainly for geo-specific checks
-      // refer to docs https://github.com/lyc8503/UptimeFlare/wiki/Check-proxy-setup before setting this value
-      // currently supports `worker://`, `globalping://` and `http(s)://` proxies
-      //checkProxy: 'https://xxx.example.com OR worker://weur',
-      // [OPTIONAL] if true, the check will fallback to local if the specified proxy is down
-      //checkProxyFallback: true,
+    },
+    {
+      id: 'expat.food',
+      name: 'Expat Food Main Page',
+      method: 'POST',
+      target: 'http://expat.food',
+      tooltip: 'This is a tooltip for this monitor',
+      statusPageLink: 'https://expat.food',
+      hideLatencyChart: false,
+      headers: {
+        'User-Agent': 'Uptimeflare',
+      },
+      body: 'Hello, world!',
     },
     // Example TCP Monitor
     {
@@ -94,9 +82,9 @@ const workerConfig: WorkerConfig = {
     // More info at Wiki: https://github.com/lyc8503/UptimeFlare/wiki/Setup-notification
     webhook: {
       // [Required] webhook URL (example: Telegram Bot API)
-      url: 'https://api.telegram.org/bot123456:ABCDEF/sendMessage',
+      url: 'https://push.pak.academy/mywebhook/publish?message=$MSG&priority=high&tags=warning,skull',
       // [Optional] HTTP method, default to 'GET' for payloadType=param, 'POST' otherwise
-      method: 'POST',
+      method: 'GET',
       // [Optional] headers to be sent
       headers: {
         foo: 'bar',
@@ -106,11 +94,11 @@ const workerConfig: WorkerConfig = {
       // 'param': append url-encoded payload to URL search parameters
       // 'json': POST json payload as body, set content-type header to 'application/json'
       // 'x-www-form-urlencoded': POST url-encoded payload as body, set content-type header to 'x-www-form-urlencoded'
-      payloadType: 'x-www-form-urlencoded',
+      payloadType: 'param',
       // [Required] payload to be sent
       // $MSG will be replaced with the human-readable notification message
       payload: {
-        chat_id: 12345678,
+        //chat_id: 12345678,
         text: '$MSG',
       },
       // [Optional] timeout calling this webhook, in millisecond, default to 5000
@@ -123,7 +111,7 @@ const workerConfig: WorkerConfig = {
     // if not specified, notification will be sent immediately
     gracePeriod: 5,
     // [Optional] disable notification for monitors with specified ids
-    skipNotificationIds: ['foo_monitor', 'bar_monitor'],
+    //skipNotificationIds: ['foo_monitor', 'bar_monitor'],
     // [Optional] suppress extra notifications for error reason changes during an incident, default to false
     skipErrorChangeNotification: true,
   },
