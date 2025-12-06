cb42b7f47cb32fb7340794c40f0c0dc9a0e5bdb7
me <me@o.o>
2025-12-06 02:35:35 +0100

    new test

--- uptime.config.ts
+++ uptime.config.ts
@@ -87,23 +87,24 @@ const workerConfig: WorkerConfig = {
       method: 'POST',
       // [Optional] headers to be sent
       headers: {
-
+        'Host': 'push.pak.academy',
+	      'Title': 'Experiencing downtime..',
+	      'Click': 'https://status.uwuu.space',
+        'Content-Type': 'application/json',
+        'Tags': 'warning,mailsrv13,daily-backup',
       },
       // [Required] Specify how to encode the payload
       // Should be one of 'param', 'json' or 'x-www-form-urlencoded'
       // 'param': append url-encoded payload to URL search parameters
       // 'json': POST json payload as body, set content-type header to 'application/json'
       // 'x-www-form-urlencoded': POST url-encoded payload as body, set content-type header to 'x-www-form-urlencoded'
-      payloadType: 'x-www-form-urlencoded',
+      payloadType: 'json',
       // [Required] payload to be sent
       // $MSG will be replaced with the human-readable notification message
       payload: {
         //chat_id: 12345678,
         //text: '$MSG',
-        Host: 'push.pak.academy',
-	      Title: 'Experiencing downtime..',
-	      Click: 'https://status.uwuu.space',
-        text: 'Hi, hi, hi, hi, hi, how are you, what do you think? is everything ok? good to see <3',
+        body: 'Hi, hi, hi, hi, hi, how are you, what do you think? is everything ok? good to see <3',
       },
       // [Optional] timeout calling this webhook, in millisecond, default to 5000
       timeout: 10000,
